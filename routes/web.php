6bab2f8b38a5be6ffb056091bf6649133aee2e85
saurbhcoder <saurbhcoder@gmail.com>
2026-03-14 10:08:31 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::post('/job/store', [JobController::class, 'store'])->name('job.store');
 
 Route::get('/sarkari-naukri', [JobController::class, 'index'])->name('jobs.index'); // List all jobs
 Route::get('/sarkari-naukri/{slug}', [JobController::class, 'show'])->name('job.show'); // Show single job
+
+Route::get('/test/env', function () {
+    dd(env('DB_DATABASE')); // Dump 'db' variable value one by one
+});
