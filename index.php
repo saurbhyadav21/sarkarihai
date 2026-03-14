c62a12101d2a78b67b0832b217c466dbbeeae0a4
saurbhcoder <saurbhcoder@gmail.com>
2026-03-14 10:11:33 +0530

    change

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
@@ -17,4 +17,4 @@ require __DIR__.'/vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/bootstrap/app.php';
 
-//$app->handleRequest(Request::capture());
+$app->handleRequest(Request::capture());
