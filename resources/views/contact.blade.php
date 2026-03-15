c7602bf79433dbcde191a8fc92a934541ba308df
saurbhcoder <saurbhcoder@gmail.com>
2026-03-15 14:28:11 +0530

    x

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container py-5">
+    <div class="container py-5" style="    color: #fff;">
 
         <h1 class="mb-4">Contact Us</h1>
 
