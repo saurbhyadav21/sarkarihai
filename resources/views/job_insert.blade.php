47bec32e816743eee506fffff02c380496852037
saurbhcoder <saurbhcoder@gmail.com>
2026-03-17 23:00:34 +0530

    g

--- resources/views/job_insert.blade.php
+++ resources/views/job_insert.blade.php
@@ -14,11 +14,31 @@
                     <label for="title" class="form-label">Job Title</label>
                     <input type="text" name="title" id="title" class="form-control" required>
                 </div>
-
+                <div class="col-md-4 mb-3">
+                    <label for="start_date" class="form-label">Start Date</label>
+                    <input type="text" name="start_date" id="start_date" class="form-control">
+                </div>
+                <div class="col-md-4 mb-3">
+                    <label for="end_date" class="form-label">End Date</label>
+                    <input type="text" name="end_date" id="end_date" class="form-control">
+                </div>
+                <div class="col-md-4 mb-3">
+                        <label for="min_salary" class="form-label">Min Salary</label>
+                        <input type="text" name="min_salary" id="min_salary" class="form-control">
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="max_salary" class="form-label">Max Salary</label>
+                        <input type="text" name="max_salary" id="max_salary" class="form-control">
+                    </div>
+                <div class="col-md-3 mb-3">
+                    <label for="min_age" class="form-label">Minimum Age</label>
+                    <input type="text" name="min_age" id="min_age" class="form-control">
+                </div>
                 <!-- Description -->
-                <div class="mb-3">
-                    <label for="desce" class="form-label">Description</label>
-                    <textarea name="desce" id="desce" class="form-control" rows="3"></textarea>
+                <div class="col-md-4 mb-3">
+                    <label class="form-label">min_qulification</label>
+                    {{-- <input type="text" name="post_eligibility" value="{{ $job->post_eligibility }}" class="form-control"> --}}
+                    <textarea name="min_qulification" class="form-control" rows="3" id="min_qulification"></textarea>
                 </div>
 
                 <div class="mb-3">
@@ -27,17 +47,20 @@
                     <input type="file" name="image" id="image" class="form-control" required>
                 </div>
 
-                
+                <div class="col-md-4 mb-3">
+                        <label for="total_vacancies" class="form-label">Total Vacancies</label>
+                        <input type="text" name="total_vacancies" id="total_vacancies" class="form-control">
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="exam_date" class="form-label">Exam Date</label>
+                        <input type="text" name="exam_date" id="exam_date" class="form-control">
+                    </div>
 
                 <!-- Dates -->
                 <div class="row">
-                    <div class="col-md-4 mb-3">
-                        <label for="start_date" class="form-label">Start Date</label>
-                        <input type="text" name="start_date" id="start_date" class="form-control">
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="end_date" class="form-label">End Date</label>
-                        <input type="text" name="end_date" id="end_date" class="form-control">
+                    <div class="mb-3">
+                        <label for="desce" class="form-label">Description</label>
+                        <textarea name="desce" id="desce" class="form-control" rows="3"></textarea>
                     </div>
                     <div class="col-md-4 mb-3">
                         <label for="last_fee_date" class="form-label">Last Date For Fee</label>
@@ -50,10 +73,7 @@
                         <label for="correction_date" class="form-label">Correction Last Date</label>
                         <input type="text" name="correction_date" id="correction_date" class="form-control">
                     </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="exam_date" class="form-label">Exam Date</label>
-                        <input type="text" name="exam_date" id="exam_date" class="form-control">
-                    </div>
+                    
                     <div class="col-md-4 mb-3">
                         <label for="admit_card" class="form-label">Admit Card Info</label>
                         <input type="text" name="admit_card" id="admit_card" class="form-control">
@@ -94,10 +114,7 @@
 
                 <!-- Age Limits -->
                 <div class="row">
-                    <div class="col-md-3 mb-3">
-                        <label for="min_age" class="form-label">Minimum Age</label>
-                        <input type="text" name="min_age" id="min_age" class="form-control">
-                    </div>
+
                     <div class="col-md-3 mb-3">
                         <label for="max_age_genral" class="form-label">Max Age (General)</label>
                         <input type="text" name="max_age_genral" id="max_age_genral" class="form-control">
@@ -114,18 +131,7 @@
 
                 <!-- Total Vacancies -->
                 <div class="row">
-                    <div class="col-md-4 mb-3">
-                        <label for="total_vacancies" class="form-label">Total Vacancies</label>
-                        <input type="text" name="total_vacancies" id="total_vacancies" class="form-control">
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="min_salary" class="form-label">Min Salary</label>
-                        <input type="text" name="min_salary" id="min_salary" class="form-control">
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="max_salary" class="form-label">Max Salary</label>
-                        <input type="text" name="max_salary" id="max_salary" class="form-control">
-                    </div>
+                    
                 </div>
 
                 <!-- Mode Selection / Posts -->
