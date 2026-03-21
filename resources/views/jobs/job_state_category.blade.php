65d20537cb694829456095e8d4690cc08b6e847a
saurbhcoder <saurbhcoder@gmail.com>
2026-03-21 15:02:38 +0530

    x

--- resources/views/jobs/job_state_category.blade.php
+++ resources/views/jobs/job_state_category.blade.php
@@ -43,17 +43,12 @@
             color: #fff;
             margin-left: -8px;
         }
-        
+
 </style>
 
 <div class="container">
     <h2 class="mb-3 c-t">
             <span><b>Latest Jobs</b></span>
-
-            <span class="last-update">
-                Last Updated : 21-03-2026 09:30
-                <img src="https://i.pinimg.com/originals/41/de/77/41de7763b09c771b14c8eb302b9bc4d2.gif">
-            </span>
         </h2>
     {{-- STATE TABS --}}
     <div class="tabs-container">
