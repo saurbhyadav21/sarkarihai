977bf6a662739a5ca331c43e62291349dc356c48
saurbhcoder <saurbhcoder@gmail.com>
2026-03-21 16:29:05 +0530

    x

--- resources/views/jobs/job_state_category.blade.php
+++ resources/views/jobs/job_state_category.blade.php
@@ -126,7 +126,7 @@
     let selectedState = @json(strtolower(trim($state ?? ''))).replace(/-/g, ' ');
     let selectedCat = @json(strtolower(trim($category ?? ''))).replace(/-/g, ' ');
 
-    console.log("URL state:", selectedState);
+    console.log("URL state:", 'Bihar');
     console.log("URL cat:", selectedCat);
 
     function filterJobs() {
