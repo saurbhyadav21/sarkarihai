edf3908d704934792fb6b03a6a122a7307615522
saurbhcoder <saurbhcoder@gmail.com>
2026-03-21 16:17:21 +0530

    x

--- resources/views/jobs/job_state_category.blade.php
+++ resources/views/jobs/job_state_category.blade.php
@@ -140,7 +140,8 @@
 
     // ✅ STATE TAB
     stateTabs.forEach(tab => {
-        console.log(tab);
+        console.log(selectedState);
+        console.log(tab.dataset.state.trim().toLowerCase());
         
         if (tab.dataset.state.trim().toLowerCase() === selectedState) {
             tab.classList.add('active');
