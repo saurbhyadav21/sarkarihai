7aa2944ffdb2733ffe7ac891097eeaa75ca17852
saurbhcoder <saurbhcoder@gmail.com>
2026-03-21 11:53:24 +0530

    x

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -398,14 +398,14 @@ $stateMap = [
 ];
 @endphp
                 <script>
-                    const data1 = [
+                    const data = [
                         @foreach($stateCounts as $state => $count)
     @if(isset($stateMap[$state]))
         ['{{ $stateMap[$state] }}', {{ $count }}],
     @endif
 @endforeach
                     ];
-                    console.log(data1);
+                    // console.log(data1);
                     
                 </script>
                 <script>
@@ -416,44 +416,44 @@ $stateMap = [
                         ).then(response => response.json());
 
 
-                        const data = [
-                            ['in-tn', 10],
-                            ['in-py', 11],
-                            ['in-hp', 12],
-                            ['in-sk', 13],
-                            ['in-dl', 14],
-                            ['in-up', 15],
-                            ['in-hr', 16],
-                            ['in-pb', 17],
-                            ['in-ch', 18],
-                            ['in-rj', 19],
-                            ['in-jk', 20],
-                            ['in-gj', 21],
-                            ['in-mp', 22],
-                            ['in-mh', 23],
-                            ['in-dh', 24],
-                            ['in-br', 25],
-                            ['in-wb', 26],
-                            ['in-jh', 27],
-                            ['in-cg', 28],
-                            ['in-od', 29],
-                            ['in-kl', 30],
-                            ['in-ka', 31],
-                            ['in-ap', 32],
-                            ['in-an', 33],
-                            ['in-as', 34],
-                            ['in-tr', 35],
-                            ['in-ar', 36],
-                            ['in-ld', 37],
-                            ['in-ml', 38],
-                            ['in-mn', 39],
-                            ['in-nl', 40],
-                            ['in-mz', 41],
-                            ['in-ts', 42],
-                            ['in-la', 43],
-                            ['in-uk', 44],
-                            ['in-ga', 45]
-                        ];
+                        // const data = [
+                        //     ['in-tn', 10],
+                        //     ['in-py', 11],
+                        //     ['in-hp', 12],
+                        //     ['in-sk', 13],
+                        //     ['in-dl', 14],
+                        //     ['in-up', 15],
+                        //     ['in-hr', 16],
+                        //     ['in-pb', 17],
+                        //     ['in-ch', 18],
+                        //     ['in-rj', 19],
+                        //     ['in-jk', 20],
+                        //     ['in-gj', 21],
+                        //     ['in-mp', 22],
+                        //     ['in-mh', 23],
+                        //     ['in-dh', 24],
+                        //     ['in-br', 25],
+                        //     ['in-wb', 26],
+                        //     ['in-jh', 27],
+                        //     ['in-cg', 28],
+                        //     ['in-od', 29],
+                        //     ['in-kl', 30],
+                        //     ['in-ka', 31],
+                        //     ['in-ap', 32],
+                        //     ['in-an', 33],
+                        //     ['in-as', 34],
+                        //     ['in-tr', 35],
+                        //     ['in-ar', 36],
+                        //     ['in-ld', 37],
+                        //     ['in-ml', 38],
+                        //     ['in-mn', 39],
+                        //     ['in-nl', 40],
+                        //     ['in-mz', 41],
+                        //     ['in-ts', 42],
+                        //     ['in-la', 43],
+                        //     ['in-uk', 44],
+                        //     ['in-ga', 45]
+                        // ];
 
 
                         Highcharts.mapChart('container', {
