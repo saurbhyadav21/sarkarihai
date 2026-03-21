83835ddf9ef17abe6b6342a409a8b4dbd0658e8c
saurbhcoder <saurbhcoder@gmail.com>
2026-03-21 15:30:34 +0530

    x

--- resources/views/jobs/job_state_category.blade.php
+++ resources/views/jobs/job_state_category.blade.php
@@ -71,9 +71,9 @@
 
                     <span><strong>{{ $job->title }}</strong></span>
                     <div style="    float: right;">
-                        <span>Salary: {{ $job->min_salary ?? 'N/A' }}-{{ $job->max_salary ?? 'N/A' }}</span>
-                        <span>Qualification: {{ $job->qualification ?? 'N/A' }}</span>
-                        <span style="color:red;">Last Date: {{ $job->last_date ?? 'Update Soon' }}</span>
+                        <span>Salary: ₹{{ $job->min_salary ?? 'N/A' }}-₹{{ $job->max_salary ?? 'N/A' }}</span>
+                        <span>Qualification: {{ $job->min_qulification ?? 'N/A' }}</span>
+                        <span style="color:red;">{{ $job->end_date ?? 'Update Soon' }}</span>
                     </div>
                 </div>
             @endforeach
