c2f895f44d987c8db38c41afe6b81a74b71de260
saurbhcoder <saurbhcoder@gmail.com>
2026-03-23 22:01:21 +0530

    x

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -81,7 +81,7 @@
     left: 50%;
     transform: translateX(-50%);
     width: 100%;
-    max-width: 1140px;
+    max-width:1340px;
     z-index: 999;
     background: #000;
     box-shadow: 0 2px 10px rgba(0,0,0,0.1);
