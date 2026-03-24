66dcb8eb11f5f13582a1d14ca0e2eaac4b69975a
saurbhcoder <saurbhcoder@gmail.com>
2026-03-24 20:31:39 +0530

    x

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -5,41 +5,34 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Response;
 use App\Models\Job;
 use Illuminate\Support\Str;
+
 class SitemapController extends Controller
 {
     public function index()
-{
-    $jobs = Job::latest()->get();
+    {
+        $jobs = Job::latest()->get();
 
-    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
-    $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
-    // Static URLs
-    $xml .= '<url>
+        $xml .= '<url>
                 <loc>' . url('/') . '</loc>
                 <priority>1.0</priority>
             </url>';
 
-    $xml .= '<url>
-                <loc>' . url('/about') . '</loc>
-                <priority>0.8</priority>
-            </url>';
+        foreach ($jobs as $job) {
+            $slug = Str::slug($job->title);
 
-    // Dynamic Jobs
-    foreach ($jobs as $job) {
-
-        $slug = Str::slug($job->title, '-');
-
-        $xml .= '<url>
+            $xml .= '<url>
                     <loc>' . url('/sarkari-naukri/' . $slug) . '</loc>
                     <lastmod>' . $job->updated_at->toAtomString() . '</lastmod>
                     <priority>0.9</priority>
                 </url>';
+        }
+
+        $xml .= '</urlset>';
+
+        return response($xml)
+            ->header('Content-Type', 'text/xml');
     }
-
-    $xml .= '</urlset>';
-
-    return response($xml, 200)
-        ->header('Content-Type', 'application/xml');
-}
 }
