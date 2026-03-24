3c5373efb28a9c79f72ca56aa71b93564e1886dc
saurbhcoder <saurbhcoder@gmail.com>
2026-03-24 20:12:45 +0530

    x

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,8 +1,7 @@
-<?xml version="1.0" encoding="UTF-8"?>
+{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-    <!-- Static Pages -->
     <url>
         <loc>{{ url('/') }}</loc>
         <priority>1.0</priority>
@@ -13,7 +12,6 @@
         <priority>0.8</priority>
     </url>
 
-    <!-- Dynamic Jobs -->
     @foreach($jobs as $job)
     <url>
         <loc>{{ url('/job/' . $job->slug) }}</loc>
