e3ee913925465b6f5943998ed4c17d9a97278e49
saurbhcoder <saurbhcoder@gmail.com>
2026-03-24 20:15:59 +0530

    x

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -10,9 +10,33 @@ class SitemapController extends Controller
     public function index()
     {
         $jobs = Job::latest()->get();
-            
-        return response()   
-            ->view('sitemap', compact('jobs'))
+
+        $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+        // Static URLs
+        $xml .= '<url>
+                <loc>' . url('/') . '</loc>
+                <priority>1.0</priority>
+            </url>';
+
+        $xml .= '<url>
+                <loc>' . url('/about') . '</loc>
+                <priority>0.8</priority>
+            </url>';
+
+        // Dynamic Jobs
+        foreach ($jobs as $job) {
+            $xml .= '<url>
+                    <loc>' . url('/job/' . $job->slug) . '</loc>
+                    <lastmod>' . $job->updated_at->toAtomString() . '</lastmod>
+                    <priority>0.9</priority>
+                </url>';
+        }
+
+        $xml .= '</urlset>';
+
+        return response($xml, 200)
             ->header('Content-Type', 'application/xml');
     }
 }
