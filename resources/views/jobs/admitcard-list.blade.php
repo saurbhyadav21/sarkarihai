29e5fb8b8be22ea6b579a4a07d03f81ef2823de2
saurbhcoder <saurbhcoder@gmail.com>
2026-03-25 23:14:28 +0530

    xxx

--- resources/views/jobs/admitcard-list.blade.php
+++ resources/views/jobs/admitcard-list.blade.php
@@ -1,12 +1,7 @@
 @extends('layouts.app')
 
-@section('title')
-    {{ $admitCard->title }} Admit Card 2026 Download
-@endsection
 
-@section('meta_description')
-    Download {{ $admitCard->title }} Admit Card 2026. Check exam dates, hall ticket and official link.
-@endsection
+
 
 @section('content')
     xxxx
