09443dba7feabb85aef954a0ddff105ddd9a0d08
saurbhcoder <saurbhcoder@gmail.com>
2026-03-26 12:22:12 +0530

    x

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -641,7 +641,20 @@
 
 
 
+    <div class="container mt-4">
+    <h2 class="mb-3 c-t">
+        <span><b>Latest State Wise Job India - 2026</b></span>
+        <span class="last-update flicker-btn">
+            Last Updated : {{ now()->format('d-m-Y H:i') }}
+            <img src="https://i.pinimg.com/originals/41/de/77/41de7763b09c771b14c8eb302b9bc4d2.gif">
+        </span>
+    </h2>
 
+    <div class="d-flex flex-column flex-md-row gap-3">
+        <div class="p-2 bg-light border">Div 1 content</div>
+        <div class="p-2 bg-light border">Div 2 content</div>
+    </div>
+</div>
 
 
     <div class="container mt-4">
