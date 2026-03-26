b5c5baebd3f439ec3202d54af7cc8d450b0dd8a2
saurbhcoder <saurbhcoder@gmail.com>
2026-03-26 15:28:36 +0530

    x

--- resources/views/jobs/resultcardshow.blade.php
+++ resources/views/jobs/resultcardshow.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('title')
-    {{ $admitCard->title }} Admit Card 2026 Download
+    {{ $resultCard->title }} Admit Card 2026 Download
 @endsection
 
 @section('meta_description')
-    Download {{ $admitCard->title }} Admit Card 2026. Check exam dates, hall ticket and official link.
+    Download {{ $resultCard->title }} Admit Card 2026. Check exam dates, hall ticket and official link.
 @endsection
 
 @section('content')
