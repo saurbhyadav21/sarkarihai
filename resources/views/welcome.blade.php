b77ec78a9d08b6dc57692ab60b05e083113e48bd
saurbhcoder <saurbhcoder@gmail.com>
2026-03-26 15:22:36 +0530

    k

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -668,42 +668,33 @@
         </h2>
         <div class="row">
             @foreach ($resultOut as $job)
-                {{-- @if (!empty($job->exam_list) && is_array($job->exam_list)) --}}
-                    {{-- @foreach ($job->exam_list as $exam) --}}
-                        <div class="col-6 col-md-5th">
-                            <a href="{{ route('result.show', $job->slug) }}" class="job-link">
+                <div class="col-6 col-md-5th">
+                    <a href="{{ route('result.show', $job->slug) }}" class="job-link">
 
-                                <div class="job-box position-relative">
+                        <div class="job-box position-relative">
 
-                                    <img src="https://media.tenor.com/UBNApyolWz4AAAAj/new-blinking-new-blinking-without-background.gif"
-                                        class="new-badge-p">
+                            <img src="https://media.tenor.com/UBNApyolWz4AAAAj/new-blinking-new-blinking-without-background.gif"
+                                class="new-badge-p">
 
-                                    <img src="https://sarkarihai.com/public/job-images/{{ $job->logo }}"
-                                        class="job-logo">
+                            <img src="https://sarkarihai.com/public/job-images/{{ $job->logo }}" class="job-logo">
 
-                                    <div>
-                                        <div class="job-title" style="font-size:12px;">
-                                            {{ $job->job_title }} <br> {{ $job->exam_dates }} - Result Out
-                                        </div>
-                                    </div>
-
+                            <div>
+                                <div class="job-title" style="font-size:12px;">
+                                    {{ $job->job_title }} <br> - Result Out
                                 </div>
+                            </div>
 
-                                <div class="job-meta">
-                                    <span style="color: green; font-weight:600;">
-                                        📝 Result Date:
-                                        {{ $exam['result_card_release_date'] ?? 'To Be Announced' }}
-                                    </span>
-                                </div>
-
-                            </a>
                         </div>
-                    {{-- @endforeach --}}
-                {{-- @else
-                    <div class="col-12">
-                        <p style="color:red;">No exam data available for this job.</p>
-                    </div>
-                @endif --}}
+
+                        <div class="job-meta">
+                            <span style="color: green; font-weight:600;">
+                                📝 Result Date:
+                                {{ $job['result_card_release_date'] ?? 'To Be Announced' }}
+                            </span>
+                        </div>
+
+                    </a>
+                </div>
             @endforeach
         </div>
     </div>
