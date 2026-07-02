5054c80962b6f4d773d8996d91d8d7f78ac4259f
saurbhcoder <saurbhcoder@gmail.com>
2026-07-03 00:14:17 +0530

    x

--- routes/console.php
+++ routes/console.php
@@ -3,31 +3,26 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-
-
+use Illuminate\Support\Facades\File;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-
-
-
 Schedule::command('jobs:fetch-news')
     ->everyMinute();
 
+Schedule::command('jobs:process-one')
+    ->everyMinute();
 
 Schedule::call(function () {
 
-    file_put_contents(
+    File::append(
         storage_path('cron_test.txt'),
-        date('Y-m-d H:i:s') . PHP_EOL,
-        FILE_APPEND
+        now() . " : Cron Working\n"
     );
+
 })->everyMinute();
 
 
 
-Schedule::command('jobs:process-one')
-    ->everyMinute();
