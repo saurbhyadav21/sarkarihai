513b5a99c8d9451a41a24d7044a119a988839fb3
saurbhcoder <saurbhcoder@gmail.com>
2026-07-03 10:55:58 +0530

    x

--- routes/web.php
+++ routes/web.php
@@ -91,11 +91,11 @@ Route::get('/latest-jobs/{state}', [JobController::class, 'latestJobs'])
 Route::get('/latest-jobs/{state}/{category}', [JobController::class, 'latestJobs'])
     ->name('latest.jobs.state.category');
 
-// Route::get('/latest-jobs/{state}/{category}/{slug}',[JobController::class, 'jobDetail'])->name('job.detail');
+Route::get('/latest-jobs/{state}/{category}/{slug}',[JobController::class, 'jobDetail'])->name('job.detail');
 
 
-Route::get('/latest-jobs/{state}/{category}/{slug}', function($state,$category,$slug){
+// Route::get('/latest-jobs/{state}/{category}/{slug}', function($state,$category,$slug){
 
-    dd($state,$category,$slug);
+//     dd($state,$category,$slug);
 
-});
+// });
