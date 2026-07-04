d4facc09da93eafba0c6bfdf34471e5e3cc756f3
saurbhcoder <saurbhcoder@gmail.com>
2026-07-05 00:16:25 +0530

    x

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -591,13 +591,13 @@ text-align:center;
                         <span class="sep">/</span>
 
                         <span class="current">
-                            {{ $title }}
+                          
                         </span>
 
                     </nav>
 
                     <h1>
-                        {{ $title }}
+                       
                     </h1>
 
                     <p>
