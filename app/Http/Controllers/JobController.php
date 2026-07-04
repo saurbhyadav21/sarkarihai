dbf60f44d02379ab4317da67402c8a6ce57ed90e
saurbhcoder <saurbhcoder@gmail.com>
2026-07-05 00:08:02 +0530

    x

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -1045,29 +1045,29 @@ class JobController extends Controller
 
 
     public function latestJobs($state = null, $category = null)
-{
-    $query = DB::table('job_details');
+    {
+        $query = DB::table('job_details');
 
-    // State filter
-    if (!empty($state)) {
-        $query->where('state', $state);
+        // State filter
+        if (!empty($state)) {
+            $query->where('state', $state);
+        }
+
+        // Category filter
+        if (!empty($category)) {
+            $query->where('category', $category);
+        }
+
+        $jobs = $query
+            ->orderBy('id', 'DESC')
+            ->paginate(20);
+
+        return view('jobs.show', [
+            'job' => $jobs,
+            'state' => $state,
+            'category' => $category,
+        ]);
     }
-
-    // Category filter
-    if (!empty($category)) {
-        $query->where('category', $category);
-    }
-
-    $jobs = $query
-        ->orderBy('id', 'DESC')
-        ->paginate(20);
-
-    return view('jobs.show', [
-        'jobs' => $jobs,
-        'state' => $state,
-        'category' => $category,
-    ]);
-}
 
     public function jobDetail($state, $category, $slug)
     {
