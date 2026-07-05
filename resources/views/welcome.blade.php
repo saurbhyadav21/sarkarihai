fe192226e1d7082e6ea30124b08b3e8055b6f4af
saurbhcoder <saurbhcoder@gmail.com>
2026-07-05 23:19:46 +0530

    x

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -141,9 +141,8 @@
                     <h1>Sarkari Result 2026</h1>
 
 <p>
-Latest Sarkari Naukri, Admit Card, Results, Answer Key, and Government Job notifications across India.
+Find the latest Sarkari Naukri 2026, Government Jobs, Admit Cards, Results, and Exam Updates in one place.
 </p>
-
 
                     <div class="row mt-4">
 
