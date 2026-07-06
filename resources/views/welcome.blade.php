3818dc281ca41e102f18fcdf9d2dd0435c277a07
saurbhcoder <saurbhcoder@gmail.com>
2026-07-06 10:47:44 +0530

    Fix URL generation for latest jobs by removing unnecessary slug conversion for states

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -720,7 +720,7 @@ PART 2 - MAIN CONTENT BLOCK
   
                     @foreach ($lastDateSoon as $job)
                         <a class="small-link" 
-                            href="{{ url('latest-jobs/' . Str::slug($states) . '/' . Str::slug($job->category) . '/' . $job->slug) }}">
+                            href="{{ url('latest-jobs/' . $states . '/' . Str::slug($job->category) . '/' . $job->slug) }}">
 
                             {{ \Illuminate\Support\Str::limit($job->title, 35) }}
 
