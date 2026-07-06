8d8030952179a9dba21892adab0a3491ff67604e
saurbhcoder <saurbhcoder@gmail.com>
2026-07-06 10:28:05 +0530

    v

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -716,7 +716,7 @@ PART 2 - MAIN CONTENT BLOCK
                 <div class="side-box mb-3"> 
 
                     <div class="side-title">Last Date Soon</div>
-
+ 
   
                     @foreach ($lastDateSoon as $job)
                         <a class="small-link" 
