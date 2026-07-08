08474127504bba5942c51b05648e6735dee31399
saurbhcoder <saurbhcoder@gmail.com>
2026-07-08 11:20:13 +0530

    x

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -792,9 +792,9 @@ PART 2 - MAIN CONTENT BLOCK
                     <div class="side-title">Last Date Soon</div>
 
                     <div class="last-date-tabs">
-                        <button class="tab-btn active" data-tab="today">Today</button>
-                        <button class="tab-btn" data-tab="tomorrow">Tomorrow</button>
-                        <button class="tab-btn" data-tab="week">7 Days</button>
+                        <button class="tab-btn active" data-tab="today">Today ({{ $todayCount }})</button>
+                        <button class="tab-btn" data-tab="tomorrow">Tomorrow ({{ $tomorrowCount }})</button>
+                        <button class="tab-btn" data-tab="week">7 Days ({{ $weekCount }})</button>
                     </div>
 
                     <div id="today" class="tab-content active">
