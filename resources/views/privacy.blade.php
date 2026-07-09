f8dea2489df58997c10beef409891357d99ac995
saurbhcoder <saurbhcoder@gmail.com>
2026-07-09 11:24:39 +0530

    x

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1525,6 +1525,19 @@ FAQ
 </div>
     </div>
 
-
+<script type="application/ld+json">
+{
+  "@context":"https://schema.org",
+  "@type":"WebPage",
+  "name":"Privacy Policy - SarkariHai",
+  "url":"https://sarkarihai.com/privacy-policy",
+  "description":"Read the Privacy Policy of SarkariHai.com to understand how we collect, use and protect your information.",
+  "publisher":{
+    "@type":"Organization",
+    "name":"SarkariHai",
+    "url":"https://sarkarihai.com"
+  }
+}
+</script>
     
 @endsection
