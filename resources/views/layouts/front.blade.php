44a74e533a987b0f6b5cb39cd1ffb077e925559a
saurbhcoder <saurbhcoder@gmail.com>
2026-07-09 11:05:25 +0530

    x

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -18,6 +18,8 @@
     <link rel="apple-touch-icon" href="https://sarkarihai.com/public/images/fevicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 
     <style>
         body {
