a3e154fa53ae53e592d06276c6d4b021642aa8d9
saurbhcoder <saurbhcoder@gmail.com>
2026-07-09 10:35:33 +0530

    x

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,22 +2,58 @@
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
+    <!-- Homepage -->
     <url>
         <loc>{{ url('/') }}</loc>
+        <changefreq>hourly</changefreq>
         <priority>1.0</priority>
     </url>
 
+    <!-- Static Pages -->
     <url>
         <loc>{{ url('/about') }}</loc>
-        <priority>0.8</priority>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
     </url>
 
-    @foreach($jobs as $job)
     <url>
-        <loc>{{ url('/job/' . $job->slug) }}</loc>
-        <lastmod>{{ $job->updated_at->toAtomString() }}</lastmod>
-        <priority>0.9</priority>
+        <loc>{{ url('/contact') }}</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
     </url>
+
+    <url>
+        <loc>{{ url('/privacy-policy') }}</loc>
+        <changefreq>yearly</changefreq>
+        <priority>0.3</priority>
+    </url>
+
+    <url>
+        <loc>{{ url('/terms-and-conditions') }}</loc>
+        <changefreq>yearly</changefreq>
+        <priority>0.3</priority>
+    </url>
+
+    <url>
+        <loc>{{ url('/disclaimer') }}</loc>
+        <changefreq>yearly</changefreq>
+        <priority>0.3</priority>
+    </url>
+
+    <!-- Jobs -->
+    @foreach($jobs as $job)
+
+    <url>
+        <loc>{{ url('sarkari-naukri/' . ($job->state ?? 'all-india') . '/' . ($job->category ?? 'government') . '/' . $job->slug) }}</loc>
+
+        <lastmod>{{ \Carbon\Carbon::parse($job->updated_at)->toAtomString() }}</lastmod>
+
+        <changefreq>daily</changefreq>
+
+        <priority>0.9</priority>
+
+    </url>
+
     @endforeach
 
 </urlset>
