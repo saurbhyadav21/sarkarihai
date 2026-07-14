3e37bb340196b9c10d8acd386ec6740fa898d0f8
saurbhcoder <saurbhcoder@gmail.com>
2026-07-14 14:30:43 +0530

    x

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -1064,71 +1064,71 @@ class JobController extends Controller
     }
 
 
-    // public function latestJobs($state = null, $category = null)
-    // {
+    public function latestJobs($state = null, $category = null)
+    {
 
-    //     $query = DB::table('job_details');
+        $query = DB::table('job_details');
 
-    //     // State filter
-    //     if (!empty($state)) {
-    //         $query->where('state', $state);
-    //     }
-
-    //     // Category filter
-    //     if (!empty($category)) {
-    //         $query->where('category', $category);
-    //     }
-
-    //     $jobs = $query
-    //         ->orderBy('id', 'DESC')
-    //         ->paginate(20);
-
-    //     return view('jobs.show', [
-    //         'jobs' => $jobs,
-    //         'state' => $state,
-    //         'category' => $category,
-    //     ]);
-    // }
-
-    public function latestJobs(
-        Request $request,
-        $state = null,
-        $category = null
-    ) {
-        $jobs = Job::query();
-
-        if ($request->filled('search')) {
-
-            $search = $request->search;
-
-            $jobs->where(function ($q) use ($search) {
-
-                $q->where('title', 'LIKE', "%{$search}%")
-                    ->orWhere('category', 'LIKE', "%{$search}%")
-                    ->orWhere('organization', 'LIKE', "%{$search}%")
-                    ->orWhere('state', 'LIKE', "%{$search}%");
-            });
+        // State filter
+        if (!empty($state)) {
+            $query->where('state', $state);
         }
 
-        if ($state && $state != 'all-india') {
-            $jobs->where('state', $state);
+        // Category filter
+        if (!empty($category)) {
+            $query->where('category', $category);
         }
 
-        if ($category) {
-            $jobs->where('category', $category);
-        }
-
-        $jobs = $jobs
-            ->latest('id')
+        $jobs = $query
+            ->orderBy('id', 'DESC')
             ->paginate(20);
 
-
-        return view('jobs.show', compact(
-            'jobs',
-            'state',
-            'category'
-        ));
+        return view('jobs.show', [
+            'jobs' => $jobs,
+            'state' => $state,
+            'category' => $category,
+        ]);
     }
+
+    // public function latestJobs(
+    //     Request $request,
+    //     $state = null,
+    //     $category = null
+    // ) {
+    //     $jobs = Job::query();
+
+    //     if ($request->filled('search')) {
+
+    //         $search = $request->search;
+
+    //         $jobs->where(function ($q) use ($search) {
+
+    //             $q->where('title', 'LIKE', "%{$search}%")
+    //                 ->orWhere('category', 'LIKE', "%{$search}%")
+    //                 ->orWhere('organization', 'LIKE', "%{$search}%")
+    //                 ->orWhere('state', 'LIKE', "%{$search}%");
+    //         });
+    //     }
+
+    //     if ($state && $state != 'all-india') {
+    //         $jobs->where('state', $state);
+    //     }
+
+    //     if ($category) {
+    //         $jobs->where('category', $category);
+    //     }
+
+    //     $jobs = $jobs
+    //         ->latest('id')
+    //         ->paginate(20);
+
+
+    //     return view('jobs.show', compact(
+    //         'jobs',
+    //         'state',
+    //         'category'
+    //     ));
+    // }
 
 
     // public function latestJobs(Request $request, $state = null, $category = null)
