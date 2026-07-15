bc651bf9a3d0eb44c0bde5fd7e32e120bf60cd0c
saurbhcoder <saurbhcoder@gmail.com>
2026-07-15 23:12:23 +0530

    x

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -20,7 +20,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet"
 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-
+    @include('seo.head')
     <style>
         body {
             background: #f5f7fb;
