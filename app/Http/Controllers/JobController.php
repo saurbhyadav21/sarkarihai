61b888a5c5f3aa935fb1ab9d51604e4bdb8026e1
saurbhcoder <saurbhcoder@gmail.com>
2026-07-16 00:00:42 +0530

    x

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -1693,7 +1693,9 @@ $robots = 'index,follow,max-image-preview:large,max-snippet:-1,max-video-preview
 $ogType = 'article';
 
 $ogImage = 'https://sarkarihai.com/public/images/logo.png?v=2';
+
 $stateName = ucfirst($job->state ?? '');
+
         return view('jobs.show_details', [
 
             'job' => $job,
