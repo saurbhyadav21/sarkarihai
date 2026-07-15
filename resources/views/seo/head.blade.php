4c6b9d59accbad1ec8eae967dafff7004f041da6
saurbhcoder <saurbhcoder@gmail.com>
2026-07-15 23:12:50 +0530

    x

---
+++ resources/views/seo/head.blade.php
@@ -0,0 +1,43 @@
+{{-- ===========================
+    META
+============================ --}}
+
+@include('seo.meta')
+
+{{-- ===========================
+    CANONICAL
+============================ --}}
+
+@include('seo.canonical')
+
+{{-- ===========================
+    ROBOTS
+============================ --}}
+
+@include('seo.robots')
+
+{{-- ===========================
+    OPEN GRAPH
+============================ --}}
+
+@include('seo.open-graph')
+
+{{-- ===========================
+    TWITTER
+============================ --}}
+
+@include('seo.twitter')
+
+{{-- ===========================
+    SCHEMA
+============================ --}}
+
+@includeWhen(isset($schemaOrganization),'seo.schema.organization')
+
+@includeWhen(isset($schemaWebsite),'seo.schema.website')
+
+@includeWhen(isset($schemaBreadcrumb),'seo.schema.breadcrumb')
+
+@includeWhen(isset($schemaJobPosting),'seo.schema.job-posting')
+
+@includeWhen(isset($schemaFaq),'seo.schema.faq')
