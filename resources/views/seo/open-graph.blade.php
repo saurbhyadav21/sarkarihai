d2e86bba96478fea44494fb0b33bbc1dac895550
saurbhcoder <saurbhcoder@gmail.com>
2026-07-15 23:26:03 +0530

    x

---
+++ resources/views/seo/open-graph.blade.php
@@ -0,0 +1,13 @@
+<meta property="og:type" content="{{ $ogType ?? 'website' }}">
+
+<meta property="og:site_name" content="SarkariHai">
+
+<meta property="og:title" content="{{ $metaTitle }}">
+
+<meta property="og:description" content="{{ $metaDescription }}">
+
+<meta property="og:url" content="{{ $canonicalUrl }}">
+
+<meta property="og:image" content="{{ $ogImage ?? asset('images/og-default.jpg') }}">
+
+<meta property="og:locale" content="en_IN">
---
+++ resources/views/seo/twitter.blade.php
@@ -0,0 +1,7 @@
+<meta name="twitter:card" content="summary_large_image">
+
+<meta name="twitter:title" content="{{ $metaTitle }}">
+
+<meta name="twitter:description" content="{{ $metaDescription }}">
+
+<meta name="twitter:image" content="{{ $ogImage ?? asset('images/og-default.jpg') }}">
