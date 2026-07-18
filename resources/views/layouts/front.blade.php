b6cfac3f3f86b37236c46dab0454969d24dca478
saurbhcoder <saurbhcoder@gmail.com>
2026-07-19 00:30:30 +0530

    x

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -20,7 +20,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet"
 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-
+    <script src="https://code.jquery.com/jquery-4.0.0.js" integrity="sha256-9fsHeVnKBvqh3FB2HYu7g2xseAZ5MlN6Kz/qnkASV8U=" crossorigin="anonymous"></script>
     @include('seo.head')
 
     <style>
