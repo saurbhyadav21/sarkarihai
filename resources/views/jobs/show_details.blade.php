ff7494b6ae250ac496ff01a1133895094e1c9e21
saurbhcoder <saurbhcoder@gmail.com>
2026-07-19 01:18:25 +0530

    x

--- resources/views/jobs/show_details.blade.php
+++ resources/views/jobs/show_details.blade.php
@@ -3598,7 +3598,7 @@
             </style>
             @php
 
-                $links = array_filter(array_map('trim', explode('#', $job->links ?? '')));
+                $links = array_filter(array_map('trim', explode('#', $job->link ?? '')));
 
                 function getLinkData($title)
                 {
