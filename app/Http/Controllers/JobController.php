3782d7f8a86f368a8f519b5fdf9189533db501bc
saurbhcoder <saurbhcoder@gmail.com>
2026-07-22 09:11:09 +0530

    X

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3006,7 +3006,7 @@ class JobController extends Controller
 
         foreach ($xml->url as $item) {
 
-            if ($count >= 10000) {
+            if ($count >= 1000) {
                 break;
             }
 
