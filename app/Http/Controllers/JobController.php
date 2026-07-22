430fdb23560feeea1a27906a81c276439bd1bbb1
saurbhcoder <saurbhcoder@gmail.com>
2026-07-22 09:28:29 +0530

    x

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3065,7 +3065,7 @@ class JobController extends Controller
         if (!$xml) {
             abort(500, 'Sitemap load failed.');
         }
-                dd($xml);
+                dd(count($xml->url));
         $urls = [];
         $count = 0;
 
