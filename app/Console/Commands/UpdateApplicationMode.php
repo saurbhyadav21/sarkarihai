2f18291810b4a75fe95589c2315a14d465d6bc3b
saurbhcoder <saurbhcoder@gmail.com>
2026-08-07 22:10:59 +0530

    x

--- app/Console/Commands/UpdateApplicationMode.php
+++ app/Console/Commands/UpdateApplicationMode.php
@@ -41,7 +41,7 @@ class UpdateApplicationMode extends Command
             ->chunk(100, function ($jobs) use (&$updated, &$failed) {
 
                 foreach ($jobs as $job) {
-
+                    dd("Processing Job ID: {$job->id} | URL: {$job->source_url}");
                     try {
 
                         $response = Http::timeout(30)
