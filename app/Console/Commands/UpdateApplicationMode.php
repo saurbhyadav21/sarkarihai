c036a04c088b1a16befdc542c230d7c28d58dcf9
saurbhcoder <saurbhcoder@gmail.com>
2026-08-07 22:57:46 +0530

    x

--- app/Console/Commands/UpdateApplicationMode.php
+++ app/Console/Commands/UpdateApplicationMode.php
@@ -39,8 +39,8 @@ class UpdateApplicationMode extends Command
             })
             ->select('id', 'source_url','source')
             ->orderBy('id')
-            // ->limit(1) // Limit the number of jobs to process in one command execution
-            ->chunk(500, function ($jobs) use (&$updated, &$failed) {
+            ->limit(1) // Limit the number of jobs to process in one command execution
+            ->chunk(1, function ($jobs) use (&$updated, &$failed) {
 
                 foreach ($jobs as $job) {
                    // dd("Processing Job ID: {$job->id}, Source: {$job->source}, URL: {$job->source_url}");
