b1bf28f9b993a3a4e59e117e674c581efac66ff5
saurbhcoder <saurbhcoder@gmail.com>
2026-08-13 13:15:29 +0530

    x

--- resources/views/jobs/last-date-soon.blade.php
+++ resources/views/jobs/last-date-soon.blade.php
@@ -627,20 +627,27 @@ text-align:center;
                 </div>
 
 
-                <div class="search-card">
+                <div class="col-lg-4 mt-4 mt-lg-0">
 
-                    <h3>
-                        Search Job
-                    </h3>
+                    <div class="search-card">
 
-                    <input type="text" placeholder="SSC, Railway, Bank">
+                        <h5 class="mb-3 fw-bold">
+                            🔍 Search Sarkari Jobs
+                        </h5>
 
-                    <button>
-                        Search
-                    </button>
+                        <div class="position-relative">
+
+                            <input type="text" id="jobSearch" class="form-control form-control-lg rounded-4 shadow-sm"
+                                placeholder="Search SSC, Railway, UPSC..." autocomplete="off">
+
+                            <div class="search-dropdown" style="display:none">
+                            </div>
+
+                        </div>
+
+                    </div>
 
                 </div>
-
             </div>
 
         </div>
