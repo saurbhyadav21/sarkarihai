e5fb2ff5d73b900e25236ade7e3178b27a14df6b
saurbhcoder <saurbhcoder@gmail.com>
2026-08-15 19:59:36 +0530

    x

--- resources/views/jobs/job-category.blade.php
+++ resources/views/jobs/job-category.blade.php
@@ -1962,7 +1962,7 @@ text-align:center;
 
                 <!-- RIGHT SEARCH -->
                
-                @include('partials.social-links')
+                @include('partial.social-links')
             </div>
 
         </div>
