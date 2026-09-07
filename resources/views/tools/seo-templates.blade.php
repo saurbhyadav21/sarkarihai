51b69a559dcca9cc9661f50e5598ea4882ad9231
saurbhcoder <saurbhcoder@gmail.com>
2026-09-07 19:52:01 +0530

    x

--- resources/views/tools/seo-templates.blade.php
+++ resources/views/tools/seo-templates.blade.php
@@ -257,7 +257,7 @@
                 <td>
                     {{ $job->state ? ucwords(str_replace('-', ' ', strtolower($job->state))) : '-' }}
                 </td> --}}
-                <td style="display: none;">
+                <td style="display: nonex;">
                     @php
                         $jobUrl = url('/sarkari-naukri/' . $job->state . '/' . $job->category . '/' . $job->slug);
 
