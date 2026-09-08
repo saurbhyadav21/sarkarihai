934877596a89785861527e415667d181338ada5d
saurbhcoder <saurbhcoder@gmail.com>
2026-09-08 11:36:04 +0530

    x

--- resources/views/tools/templates.blade.php
+++ resources/views/tools/templates.blade.php
@@ -57,7 +57,7 @@
             <th>Job 2 - Salary</th>
             <th>Job 2 - Age</th>
             <th>Job 2 - State</th>
-            <th>Job 2 - YouTube Description</th>
+            {{-- <th>Job 2 - YouTube Description</th> --}}
         </tr>
     </thead>
 
